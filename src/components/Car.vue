b04172329df4c75a6a1e87bede0d2d1c706a03f3
zatulenko_d <d.zatulenko@belitsoft.com>
2019-12-05 18:20:37 +0300

    Добавлен урок 32

---
+++ src/components/Car.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="car">
+        <h2>Model: {{model}}</h2>
+        <h2>Year: {{year}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        props:{
+            model: String,
+            year:Number
+        }
+    }
+</script>
+
+<style scoped>
+.car{
+    border: 1px solid green;
+    padding: 5px;
+}
+</style>
