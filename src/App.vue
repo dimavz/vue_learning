8c50af893e0babc8ca19d7f537adc939f71f0a23
zatulenko_d <d.zatulenko@belitsoft.com>
2019-12-05 18:20:37 +0300

    Добавлен урок 32

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-<!--    <img alt="Vue logo" src="./assets/logo.png">-->
-    <MyComponent msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!--    <img alt="Vue logo" src="./assets/logo.png">-->
+        <MyComponent msg="Welcome to Your Vue.js App"/>
+        <Car :model="model" :year="year"></Car>
+        <Car :model="model" :year="year"></Car>
+    </div>
 </template>
 
 <script>
-import MyComponent from './components/MyComponent.vue'
+    import MyComponent from './components/MyComponent.vue'
+    import Car from './components/Car.vue'
 
-export default {
-  name: 'app',
-  components: {
-    MyComponent
-  }
-}
+    export default {
+        name: 'app',
+        data() {
+            return {
+                model: 'Ford',
+                year: 2017
+            }
+        },
+        components: {
+            MyComponent, Car
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Car.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="car">
+        <h2>Model: {{model}}</h2>
+        <h2>Year: {{year}}</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Car",
+        props:{
+            model: String,
+            year:Number
+        }
+        // props:['model','year']
+    }
+</script>
+
+<style scoped>
+.car{
+    border: 1px solid green;
+    padding: 5px;
+}
+</style>
